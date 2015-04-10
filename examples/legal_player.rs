ae69302f0bcec706c84d8560c49b6cea0a87e0b0
Gulshan Singh <gulshan@umich.edu>
2015-04-10 12:27:33 -0400

    Added legal player

--- examples/legal_player.rs
+++ examples/legal_player.rs
@@ -1,25 +1,23 @@
-extern crate rand;
 extern crate ggp_rs;
 
 use ggp_rs::{Player, Game, Move};
 use std::net::Ipv4Addr;
 
-struct RandomPlayer;
+struct LegalPlayer;
 
-impl Player for RandomPlayer {
+impl Player for LegalPlayer {
     fn get_name(&self) -> String {
-        "RandomPlayer".to_string()
+        "LegalPlayer".to_string()
     }
 
     fn select_move(&self, game: &Game) -> Move {
         let state = game.get_current_state();
         let role = game.get_role();
         let mut moves = game.get_legal_moves(state, role);
-        let r = rand::random::<usize>() % moves.len();
-        moves.swap_remove(r)
+        moves.swap_remove(0)
     }
 }
 
 fn main() {
-    ggp_rs::run((Ipv4Addr::new(0,0,0,0), 9147), RandomPlayer);
+    ggp_rs::run((Ipv4Addr::new(0,0,0,0), 9147), LegalPlayer);
 }
